ceef6c75a7510cb0d3fafaef46981bcdc6b5b229
Anjon Mir <anjonmir@gmail.com>
2024-04-20 02:37:26 +0600

    week2 complited

---
+++ lib/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(child: Text('Easy Parent')),
+          backgroundColor: Colors.blue,
+          
+        ),
+        body: Center(
+          child: Text("Our easy parent apps"),
+        ),
+        drawer: Drawer(),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+// import 'dart:ffi';
+
+import 'package:easy_parents/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
-
 
 class MyApp extends StatelessWidget {
   const MyApp();
@@ -11,14 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text('Easy Parent')),
-        ),
-        body: Center(
-          child: Text("Our easy parent apps"),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
